4c34a2e467923ea63e903a3d633587741a469570
Alexey Tsarenko <alzozp@gmail.com>
2020-09-06 09:59:02 +0300

    working on third task

--- src/main/java/com/springApp/service/RequestProcessingService.java
+++ src/main/java/com/springApp/service/RequestProcessingService.java
@@ -4,6 +4,7 @@ import com.springApp.config.PropertyReader;
 import com.springApp.entities.RequestEntity;
 import com.springApp.repositories.RequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -11,28 +12,35 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
-import java.util.Properties;
+import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.stream.IntStream;
 
 @Service
-public class RequestProcessingService{
+@Scope("prototype")
+public class RequestProcessingService implements Runnable{
     private final RequestRepository requestRepository;
     private final WebClient webClient;
     private final TaskExecutor taskExecutor;
+    private static Object obj = new Object();
     @Autowired
     RequestProcessingService(RequestRepository requestRepository, WebClient.Builder webClientBuilder, TaskExecutor taskExecutor){
         //this.start();
         this.taskExecutor = taskExecutor;
         this.requestRepository = requestRepository;
         this.webClient = webClientBuilder.baseUrl(PropertyReader.getProperties("server.url")).build();
-        this.taskExecutor.execute(this::run);
+      //  int threadAmount = Integer.parseInt(PropertyReader.getProperties("thread.amount"));
 
-        Thread requestRepair = new Thread(this::repair);
+        //for(int i = 0; i < 3; i++) {
+          //  this.taskExecutor.execute(this::runTask);\
+        //    this.taskExecutor.execute(new SomeTask(requestRepository));
+      //  }
+      //  Thread requestRepair = new Thread(this::repair);
+      //  requestRepair.start();
     }
-    private void repair(){
+    /*private void repair(){
         try {
-            Thread.sleep(60000);
+            Thread.sleep(20000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -40,23 +48,32 @@ public class RequestProcessingService{
             List<RequestEntity> reqList= requestRepository.findAllByStatus("STARTEDTOPROCESS");
             reqList.forEach(this::makeCallToProcessingService);
         }
-    }
-    @Async
+    }*/
+    @Override
     public void run(){
         for(;;) {
             try {
-                Thread.sleep(5000);
+                Thread.sleep(10000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            Optional<RequestEntity> optional = requestRepository.findFirstByStatusOrStatus("UNPROCESSED", "PROCESSING");
 
-            if (optional.isPresent()) {
-                System.out.println("in the thread");
-                RequestEntity requestEntity = optional.get();
-                requestEntity.setStatus("STARTEDTOPROCESS");
-                makeCallToProcessingService(requestEntity);
-            }else{
+            Optional<RequestEntity> optional;
+            synchronized (obj) {
+                 optional = requestRepository.findFirstByStatusOrStatus("UNPROCESSED", "PROCESSING");
+
+                if (optional.isPresent()) {
+                    System.out.println("in the thread" + Thread.currentThread().getName());
+                    System.out.println("working with " + optional.get().getId());
+                    RequestEntity requestEntity = optional.get();
+                    requestEntity.setStatus("STARTEDTOPROCESS");
+                    System.out.println("end" + Thread.currentThread().getName());
+                }
+            }
+            if(optional.isPresent()) {
+                makeCallToProcessingService(optional.get());
+            }
+            else{
                 System.out.println("no requests " + Thread.currentThread().getName());
             }
         }
